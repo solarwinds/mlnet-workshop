01ce8646cc9b153b6926d7d984c4c133aebd8e39
Dan Jagnow <dan.jagnow@gmail.com>
2019-11-29 23:10:15 -0600

    Added a TimeSeriesAnalysis class
    Added a TimeSeriesAnalysis class to group the raw time series data with predictions (eventually).

---
+++ src/TimeSeriesAnalysis.cs
@@ -0,0 +1,12 @@
+namespace Anomalies
+{
+    internal class TimeSeriesAnalysis
+    {
+        public TimeSeries TimeSeries { get; }
+
+        public TimeSeriesAnalysis(TimeSeries timeSeries)
+        {
+            TimeSeries = timeSeries;
+        }
+    }
+}
