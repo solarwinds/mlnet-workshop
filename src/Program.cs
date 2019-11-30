455f5de3c133626e620ae6397fb51748323d310e
Dan Jagnow <dan.jagnow@gmail.com>
2019-11-29 23:29:50 -0600

    Step 3: Display a Graph
    Completed Step 3 by updating the Program class to generate a TimeSeriesAnalysis for each TimeSeries and then to create and show charts for each of these.

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using XPlot.Plotly;
 
 namespace Anomalies
 {
@@ -7,7 +9,31 @@ namespace Anomalies
         static void Main(string[] args)
         {
             TimeSeries[] stockSeries = StockLoader.Load();
+            IEnumerable<TimeSeriesAnalysis> analysisResults = Analyze(stockSeries);
+            ShowCharts(analysisResults);
             Console.WriteLine("Finished!");
+        }
+
+        private static IEnumerable<TimeSeriesAnalysis> Analyze(IEnumerable<TimeSeries> timeSeriesList)
+        {
+            foreach (TimeSeries timeSeries in timeSeriesList)
+            {
+                var analysis = new TimeSeriesAnalysis(timeSeries);
+                yield return analysis;
+            }
+        }
+
+        private static void ShowCharts(IEnumerable<TimeSeriesAnalysis> analysisResults)
+        {
+            var charts = new List<PlotlyChart>();
+
+            foreach (TimeSeriesAnalysis analysis in analysisResults)
+            {
+                PlotlyChart chart = ChartBuilder.BuildChart(analysis);
+                charts.Add(chart);
+            }
+
+            Chart.ShowAll(charts);
         }
     }
 }
