763c70ceced0423d704994ae55fb55dccbce574a
Dan Jagnow <dan.jagnow@gmail.com>
2019-11-29 19:37:46 -0600

    Step 1: Create a Project
    Created a new command line project and added NuGet packages.

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Anomalies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
